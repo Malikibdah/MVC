66213b817a3bfcc7488b1adf0b41c6d101ada812
Malikibdah <malikibdah88@gmail.com>
2024-07-23 21:46:13 +0300

    23.07.2024task2

---
+++ task2/WebApplication2test/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+           
+
+            return View();
+        }
+        public ActionResult Contact()
+        {
+
+
+            return View();
+        }
+        public ActionResult Contactrview()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Contact(FormCollection form)
+        {
+
+            
+            ViewBag.Name=form["name"];
+            ViewBag.Birthyear = form["birth"];
+            ViewBag.Gender = form["gender"];
+            ViewBag.Courses = form["courses"];
+            ViewBag.Skills = form["skills"];
+
+
+
+
+            return View("Contactrview");
+        }
+    }
+}
---
+++ task2/WebApplication2test/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2test.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult login()
+        {
+            return View();
+        }
+        public ActionResult Regest()
+        {
+            ViewBag.intername = "Please Inter Your Name : ";
+            ViewBag.interemail = "Please Inter Your Email : ";
+            Session["interpass"] = "Inter Your Password : ";
+
+            return View();
+
+        }
+        public ActionResult feedback()
+        {
+            ViewData["intername"] = "Please Inter Your Name : ";
+            ViewData["interemail"] = "Please Inter Your Email : ";
+            ViewData["interemessage"] = "Please Inter Your Message : ";
+            TempData["moreinfo"] = "For more inrformation get touch with use using Number : 0786217123";
+
+
+            return View();
+
+        }
+        public ActionResult home()
+        {
+            return View();
+        }
+    }
+}
